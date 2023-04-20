ab3564f61433d555ef72b71a1a7cabc24694f4a4
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-04-20 22:10:32 +0530

    Adding index, style and script files

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(function (panel) {
+  panel.addEventListener("click", function () {
+    removeAvtiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeAvtiveClasses() {
+  panels.forEach(function (panel) {
+    panel.classList.remove("active");
+  });
+}
